261aba94a91ac9b3e8de7786c8d3e01508cedc1e
Ryan Urabe <rurabe@gmail.com>
2014-03-26 21:03:32 -0600

    ok more requires

--- lib/chairnerd.rb
+++ lib/chairnerd.rb
@@ -2,7 +2,12 @@ require 'json'
 require 'uri'
 require 'net/http'
 
-Gem.find_files("chairnerd/**/*.rb").each { |path| require path }
+require 'chairnerd/client'
+require 'chairnerd/event'
+require 'chairnerd/response/base'
+require 'chairnerd/response/collection_response'
+require 'chairnerd/response/event_response'
+require 'chairnerd/response/events_response'
 
 module Chairnerd
 end
--- lib/chairnerd/version.rb
+++ lib/chairnerd/version.rb
@@ -1,3 +1,3 @@
 module Chairnerd
-  VERSION = "0.0.2"
+  VERSION = "0.0.4"
 end
